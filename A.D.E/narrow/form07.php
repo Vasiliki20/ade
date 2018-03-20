af656e86f0dfb64a5d0a730ddc65b7db7d588114
Vasiliki <vpante02@cs.ucy.ac.cy>
2018-03-20 21:04:25 +0200

    forma7

--- A.D.E/narrow/form07.php
+++ A.D.E/narrow/form07.php
@@ -384,22 +384,12 @@
 		</div>
 	</body>
 </html>
-
 <?php
-
-if (isset($_POST['submit'])){
-$request = new HttpRequest();
-$request->setUrl('http://localhost/mhcserver/post/form7.php');
-$request->setMethod(HTTP_METH_POST);
-
-$request->setHeaders(array(
-  'cache-control' => 'no-cache',
-  'content-type' => 'application/x-www-form-urlencoded',
-  'authorization' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTg3LCJleHAiOjE1MTgyNzI4NzV9.J90clNUiOoVLnqc9ND_mivBdf7mtxtL6BoE3yEYpQ2c'
-));
-
-$request->setContentType('application/x-www-form-urlencoded');
-$request->setPostFields(array(
+require_once("requests.php");
+$url="http://thesis.in.cs.ucy.ac.cy/mhc/mhcserver/post/form7.php";
+$method='POST';
+if(isset($_POST['submit'])){
+$postfields=http_build_query(array(
   'patientID' => $_POST['id'],
   'mainproblem' => $_POST['mainproblem'],
   'historyofproblem' => $_POST['historyofproblem'],
@@ -421,15 +411,24 @@ $request->setPostFields(array(
   'signed' => $_POST['signed'],
   'datecompleted' => $_POST['datecompleted']
 ));
-
-try {
-  $response = $request->send();
-
-  echo $response->getBody();
-} catch (HttpException $ex) {
-  echo $ex;
+	if(isset($_COOKIE['token'])){
+		$response=request($url,$method,$postfields,$_COOKIE['token']);
+	}else{
+		$response=0;
+	}
+	while($response['status']!=1){
+		$tok=giveToken();
+		print "<h5>".$tok."</h5>";
+		?>
+		<script>
+			document.cookie='token=<?= $tok ?>';
+		</script>
+		<?php
+		//$GLOBALS['curtoken']=giveToken();
+		//print "<h5>".$GLOBALS['curtoken']."</h5>";
+		$response=request($url,$method,$postfields,$tok);
+	}
+	
 }
-
-}
-
 ?>
+
