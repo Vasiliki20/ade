d286cf7300179be4f27ad2165e896ede491766f9
Vasiliki <vpante02@cs.ucy.ac.cy>
2018-03-20 21:10:07 +0200

    upd

--- A.D.E/narrow/form12.php
+++ A.D.E/narrow/form12.php
@@ -269,20 +269,11 @@
 </html>
 
 <?php
-
-if (isset($_POST['submit'])){
-$request = new HttpRequest();
-$request->setUrl('http://localhost/mhcserver/post/form12.php');
-$request->setMethod(HTTP_METH_POST);
-
-$request->setHeaders(array(
-  'cache-control' => 'no-cache',
-  'content-type' => 'application/x-www-form-urlencoded',
-  'authorization' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTg3LCJleHAiOjE1MTgyNzI4NzV9.J90clNUiOoVLnqc9ND_mivBdf7mtxtL6BoE3yEYpQ2c'
-));
-
-$request->setContentType('application/x-www-form-urlencoded');
-$request->setPostFields(array(
+require_once("requests.php");
+$url="http://thesis.in.cs.ucy.ac.cy/mhc/mhcserver/post/form12.php";
+$method='POST';
+if(isset($_POST['submit'])){
+$postfields=http_build_query(array(
   'id' => $_POST['id'],
   'typeof' => $_POST[''],
   'nameofprof' => $_POST['nameofprof'],
@@ -294,14 +285,24 @@ $request->setPostFields(array(
   'infototransfer' => $_POST['infototransfer'],
   'timeofexpire' => $_POST['timeofexpire'],
   'signedinfo' => $_POST['afora']
-));
-
-try {
-  $response = $request->send();
-
-  echo $response->getBody();
-} catch (HttpException $ex) {
-  echo $ex;
+	));
+	if(isset($_COOKIE['token'])){
+		$response=request($url,$method,$postfields,$_COOKIE['token']);
+	}else{
+		$response=0;
+	}
+	while($response['status']!=1){
+		$tok=giveToken();
+		print "<h5>".$tok."</h5>";
+		?>
+		<script>
+			document.cookie='token=<?= $tok ?>';
+		</script>
+		<?php
+		//$GLOBALS['curtoken']=giveToken();
+		//print "<h5>".$GLOBALS['curtoken']."</h5>";
+		$response=request($url,$method,$postfields,$tok);
+	}
+	
 }
-
-}
+?>
