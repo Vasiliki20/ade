e65c420d9392f71a35a6a57178c1b45eeaeaf620
epapad04 <epapad04@cs.ucy.ac.cy>
2018-03-23 19:01:39 +0200

    upd

---
+++ server/post/psylogin.php
@@ -0,0 +1,14 @@
+<?php
+ include('post.php');
+ if(isset($_POST['id']) && isset($_POST['password'])){
+	 $stmt = $db->prepare('SELECT password FROM psychologist WHERE psychologistID=:psychologistID');
+	 $stmt->bindParam(':psychologistID',$_POST['id']);
+	 $stmt->execute();
+	 $result = $stmt->fetch(PDO::FETCH_ASSOC);
+	 if(strnatcmp($result['password'],$_POST['password'])==0){
+		 $response['login']=true; $response['success']=1; echo json_encode($response);//echo "\nTrue credentials!";
+	 }else{
+		 $response['login']=false; $response['success']=1; echo json_encode($response); //echo "\nFalse credentials!";
+	 }
+ }else{$response['login']=false; $response['success']=0; $response['error']="Wrong Fields!"; echo json_encode($response);}
+ ?>
