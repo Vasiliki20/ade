13aa729b3d22e027986caa68ecf0c5b4a78aa812
epapad04 <epapad04@cs.ucy.ac.cy>
2018-03-23 15:13:38 +0200

    upd

--- server/post/psyregister.php
+++ server/post/psyregister.php
@@ -1,14 +1,22 @@
 <?php
  include('post.php');
- if(isset($_POST['id']) && isset($_POST['email']) && isset($_POST['name']) && isset($_POST['lastname']) &&  isset($_POST['password'])){
+ if(isset($_POST['id']) && isset($_POST['name']) && isset($_POST['lastname']) && isset($_POST['sex']) && isset($_POST['age']) && isset($_POST['address']) && isset($_POST['telephone']) && isset($_POST['fax']) && isset($_POST['position']) && isset($_POST['speciality']) && isset($_POST['building']) && isset($_POST['office_num']) && isset($_POST['email']) && isset($_POST['password'])){
  makenull();
- $name=$_POST['name'];
  try{ 
- $stmt = $db->prepare('INSERT INTO psychologist(psychologistID,email,firstname,lastname,password) VALUES(:id,:email,:name,:lastname,:password)');
+ $stmt = $db->prepare('INSERT INTO psychologist(psychologistID,firstname,lastname,sex,age,address,telephone,fax,position,speciality,building,office_num,email,password) VALUES(:id,:name,:lastname,:sex,:age,:address,:telephone,:fax,:position,:speciality,:building,:office_num,:email,:password)');
  $stmt->bindParam(':id',$_POST['id']);
- $stmt->bindParam(':email',$_POST['email']);
  $stmt->bindParam(':name',$_POST['name']);
  $stmt->bindParam(':lastname',$_POST['lastname']);
+ $stmt->bindParam(':sex',$_POST['sex']);
+ $stmt->bindParam(':age',$_POST['age']);
+ $stmt->bindParam(':address',$_POST['address']);
+ $stmt->bindParam(':telephone',$_POST['telephone']);
+ $stmt->bindParam(':fax',$_POST['fax']);
+ $stmt->bindParam(':position',$_POST['position']);
+ $stmt->bindParam(':speciality',$_POST['speciality']);
+ $stmt->bindParam(':building',$_POST['building']);
+ $stmt->bindParam(':office_num',$_POST['office_num']);
+ $stmt->bindParam(':email',$_POST['email']);
  $stmt->bindParam(':password',$_POST['password']);
  $stmt->execute();
  }catch(PDOException $e){$response['success']=0; $response['sqlerror']=$e->getMessage(); echo json_encode($response); exit();}
