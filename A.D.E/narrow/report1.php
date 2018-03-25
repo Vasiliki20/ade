4dcef863092524e8b80f41bf47525ad05ac21279
Vasiliki <vpante02@cs.ucy.ac.cy>
2018-03-25 02:23:14 +0200

    newinterface

---
+++ A.D.E/narrow/report1.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Κέντρο Ψυχικής Υγείας</title>
+
+		<!-- Bootstrap Core CSS -->
+		<link href="bootstrap/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- MetisMenu CSS -->
+		<link href="bootstrap/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+		<!-- DataTables CSS -->
+		<link href="bootstrap/vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+		<!-- DataTables Responsive CSS -->
+		<link href="bootstrap/vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+
+		<!-- Custom CSS -->
+		<link href="bootstrap/dist/css/sb-admin-2.css" rel="stylesheet">
+
+		<!-- Custom Fonts -->
+		<link href="bootstrap/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	</head>
+	<script>
+		$(document).ready(function() {
+			$('myTable').DataTable();
+		});
+	</script>
+
+	<style>
+		table, th, td {
+			border: 1px solid black;
+			border-collapse: collapse;
+		}
+		th, td {
+			padding: 5px;
+			text-align: left;
+		}
+		label {
+			display: inline-block;
+			width: 140px;
+			text-align: left;
+			float: left;
+		}​
+
+		input {
+			display: inline-block;
+			float: right;
+			text-align: right;
+		}
+	</style>
+
+	<body>
+
+		<div id="wrapper">
+
+			<!-- Navigation -->
+			<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="psindex.php">Κέντρο Ψυχικής Υγείας</a>
+				</div>
+				<!-- /.navbar-header -->
+
+				<ul class="nav navbar-top-links navbar-right">
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#"> <i class="fa fa-envelope fa-fw"></i> <i class="fa fa-caret-down"></i> </a>
+						<ul class="dropdown-menu dropdown-messages">
+							<li>
+								<a href="#">
+								<div>
+									<strong>John Smith</strong>
+									<span class="pull-right text-muted"> <em>Yesterday</em> </span>
+								</div>
+								<div>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...
+								</div> </a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="#">
+								<div>
+									<strong>John Smith</strong>
+									<span class="pull-right text-muted"> <em>Yesterday</em> </span>
+								</div>
+								<div>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...
+								</div> </a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="#">
+								<div>
+									<strong>John Smith</strong>
+									<span class="pull-right text-muted"> <em>Yesterday</em> </span>
+								</div>
+								<div>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...
+								</div> </a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a class="text-center" href="#"> <strong>Read All Messages</strong> <i class="fa fa-angle-right"></i> </a>
+							</li>
+						</ul>
+						<!-- /.dropdown-messages -->
+					</li>
+					<!-- /.dropdown -->
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#"> <i class="fa fa-tasks fa-fw"></i> <i class="fa fa-caret-down"></i> </a>
+						<ul class="dropdown-menu dropdown-tasks">
+							<li>
+								<a href="#">
+								<div>
+									<p>
+										<strong>Task 1</strong>
+										<span class="pull-right text-muted">40% Complete</span>
+									</p>
+									<div class="progress progress-striped active">
+										<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+											<span class="sr-only">40% Complete (success)</span>
+										</div>
+									</div>
+								</div> </a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="#">
+								<div>
+									<p>
+										<strong>Task 2</strong>
+										<span class="pull-right text-muted">20% Complete</span>
+									</p>
+									<div class="progress progress-striped active">
+										<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+											<span class="sr-only">20% Complete</span>
+										</div>
+									</div>
+								</div> </a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="#">
+								<div>
+									<p>
+										<strong>Task 3</strong>
+										<span class="pull-right text-muted">60% Complete</span>
+									</p>
+									<div class="progress progress-striped active">
+										<div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+											<span class="sr-only">60% Complete (warning)</span>
+										</div>
+									</div>
+								</div> </a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="#">
+								<div>
+									<p>
+										<strong>Task 4</strong>
+										<span class="pull-right text-muted">80% Complete</span>
+									</p>
+									<div class="progress progress-striped active">
+										<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+											<span class="sr-only">80% Complete (danger)</span>
+										</div>
+									</div>
+								</div> </a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a class="text-center" href="#"> <strong>See All Tasks</strong> <i class="fa fa-angle-right"></i> </a>
+							</li>
+						</ul>
+						<!-- /.dropdown-tasks -->
+					</li>
+					<!-- /.dropdown -->
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#"> <i class="fa fa-bell fa-fw"></i> <i class="fa fa-caret-down"></i> </a>
+						<ul class="dropdown-menu dropdown-alerts">
+							<li>
+								<a href="#">
+								<div>
+									<i class="fa fa-comment fa-fw"></i> New Comment <span class="pull-right text-muted small">4 minutes ago</span>
+								</div> </a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="#">
+								<div>
+									<i class="fa fa-twitter fa-fw"></i> 3 New Followers <span class="pull-right text-muted small">12 minutes ago</span>
+								</div> </a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="#">
+								<div>
+									<i class="fa fa-envelope fa-fw"></i> Message Sent <span class="pull-right text-muted small">4 minutes ago</span>
+								</div> </a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="#">
+								<div>
+									<i class="fa fa-tasks fa-fw"></i> New Task <span class="pull-right text-muted small">4 minutes ago</span>
+								</div> </a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="#">
+								<div>
+									<i class="fa fa-upload fa-fw"></i> Server Rebooted <span class="pull-right text-muted small">4 minutes ago</span>
+								</div> </a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a class="text-center" href="#"> <strong>See All Alerts</strong> <i class="fa fa-angle-right"></i> </a>
+							</li>
+						</ul>
+						<!-- /.dropdown-alerts -->
+					</li>
+					<!-- /.dropdown -->
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#"> <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i> </a>
+						<ul class="dropdown-menu dropdown-user">
+							<li>
+								<a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="login.html"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+							</li>
+						</ul>
+						<!-- /.dropdown-user -->
+					</li>
+					<!-- /.dropdown -->
+				</ul>
+				<!-- /.navbar-top-links -->
+				<div class="navbar-default sidebar" role="navigation">
+					<div class="sidebar-nav navbar-collapse">
+						<ul class="nav" id="side-menu">
+							<!--
+							<li class="sidebar-search">
+							<div class="input-group custom-search-form">
+							<input type="text" class="form-control" placeholder="Search...">
+							<span class="input-group-btn">
+							<button class="btn btn-default" type="button">
+							<i class="fa fa-search"></i>
+							</button> </span>
+							</div>
+							<!-- /input-group -->
+							<!--</li>-->
+							<li>
+								<a href="psindex.php"><i class="fa fa-table"></i> Calendar</a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-list"></i> Open<span class="fa arrow"></span></a>
+								<ul class="nav nav-second-level">
+									<li>
+										<a href="tasklistfinal.php">Task List</a>
+									</li>
+									<li>
+										<a href="myclientsfinal.php">My clients</a>
+									</li>
+									<li>
+										<a href="#">Approve Incoming Data</a>
+									</li>
+									<li>
+										<a href="#">Notes</a>
+									</li>
+									<li>
+										<a href="waitinglistfinal.php">Waiting List</a>
+									</li>
+									<li>
+										<a href="searchfinal.php">Search</a>
+									</li>
+								</ul>
+								<!-- /.nav-second-level -->
+							</li>
+							<li>
+								<a href="reportsfinal.php"><i class="fa fa-bar-chart-o"></i> Reports</a>
+							</li>
+							<li>
+								<a href="helpfinal.php"><i class="fa fa-cog"></i> Help</a>
+							</li>
+						</ul>
+					</div>
+					<!-- /.sidebar-collapse -->
+				</div>
+				<!-- /.navbar-static-side -->
+			</nav>
+			<div id="page-wrapper">
+				<div class="row">
+					<div class="col-lg-12">
+						<h1 class="page-header">Report 1</h1>
+					</div>
+					<!-- /.col-lg-12 -->
+				</div>
+				<!-- /.row -->
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						Μέσος Όρος Γυναικών
+						<br>
+					</div>
+					<div>
+						<br>
+					</div>
+
+					<div class="form-group">
+						<label for="yearstart"><b> &nbsp;Χρονιά Έναρξης:</b></label>
+						<input type="number" class="form-control" id="yearstart" placeholder="" name="yearstart">
+					</div>
+					<div class="form-group">
+						<label for="yearend"><b> &nbsp;Χρονιά Λήξης:</b></label>
+						<input type="number" class="form-control" id="yearend" placeholder="" name="yearend">
+					</div>
+					<button type="submit" class="btn btn-default" name="submit">
+						 &nbsp;OK
+					</button>
+				</div>
+
+				<!-- /#wrapper -->
+
+				<!-- jQuery -->
+				<script src="bootstrap/vendor/jquery/jquery.min.js"></script>
+
+				<!-- Bootstrap Core JavaScript -->
+				<script src="bootstrap/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+				<!-- Metis Menu Plugin JavaScript -->
+				<script src="bootstrap/vendor/metisMenu/metisMenu.min.js"></script>
+
+				<!-- DataTables JavaScript -->
+				<script src="bootstrap/vendor/datatables/js/jquery.dataTables.min.js"></script>
+				<script src="bootstrap/vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+				<script src="bootstrap/vendor/datatables-responsive/dataTables.responsive.js"></script>
+
+				<!-- Custom Theme JavaScript -->
+				<script src="bootstrap/dist/js/sb-admin-2.js"></script>
+
+				<!-- Page-Level Demo Scripts - Tables - Use for reference -->
+				<script>
+					$(document).ready(function() {
+						$('#dataTables-example').DataTable({
+							responsive : true
+						});
+					});
+				</script>
+
+	</body>
+
+</html>
