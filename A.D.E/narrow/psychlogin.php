03783d88e4e05a8b506e20007404c10ae495a0a7
Vasiliki <vpante02@cs.ucy.ac.cy>
2018-03-25 02:46:44 +0200

    newinterface

--- A.D.E/narrow/psychlogin.php
+++ A.D.E/narrow/psychlogin.php
@@ -1,271 +1,96 @@
-<!doctype html>
-<html lang=''>
+<!DOCTYPE html>
+<html lang="en">
+
 	<head>
-		<meta charset='utf-8'>
+
+		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="styles.css">
-		<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-		<script src="script.js"></script>
 		<title>Κέντρο Ψυχικής Υγείας</title>
+
+		<!-- CSS -->
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+		<link rel="stylesheet" href="bootzard-bootstrap-wizard-template/assets/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="bootzard-bootstrap-wizard-template/assets/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="bootzard-bootstrap-wizard-template/assets/css/form-elements.css">
+		<link rel="stylesheet" href="bootzard-bootstrap-wizard-template/assets/css/style.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- Favicon and touch icons -->
+		<!--	<link rel="shortcut icon" href="bootzard-bootstrap-wizard-template/assets/ico/favicon.png">
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="bootzard-bootstrap-wizard-template/assets/ico/apple-touch-icon-144-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="bootzard-bootstrap-wizard-template/assets/ico/apple-touch-icon-114-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="bootzard-bootstrap-wizard-template/assets/ico/apple-touch-icon-72-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" href="bootzard-bootstrap-wizard-template/assets/ico/apple-touch-icon-57-precomposed.png">
+		-->
 	</head>
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-	<!------ Include the above in your HEAD tag ---------->
 
 	<style>
-		html, body {
-			height: 100%;
-			width: 100%;
-		}
-
 		body {
-			font-family: 'Raleway', sans-serif;
-			position: relative;
-			/*background: rgba(0, 0, 0, 0) url("../img/ptn.png") repeat scroll 0 0;*/
-		}
-
-		.in-page {
-			min-height: 520px;
-		}
-
-		.main {
-			position: relative;
-		}
-
-		a {
-			color: #1b5a7c;
-		}
-
-		a:hover, a:focus {
-			color: #1b5a7c;
-		}
-
-		.btn-cyan {
-			background-color: #1b5a7c;
-			color: #fff;
-		}
-
-		.btn-cyan:hover {
-			color: #fff;
-			opacity: 0.9;
-		}
-
-		.form-control:focus {
-			border-color: #1b5a7c;
-			box-shadow: 0 1px 1px rgba(0, 0, 0, 0.075) inset, 0 0 8px rgba(24, 204, 162, 0.6);
-			outline: 0 none;
-		}
-
-		.min-height {
-			min-height: 380px;
-		}
-
-		.login-screen {
-			background-image: url(https://www.squiz.net/__public/Systems/Perspective/FileCache/1-32000/465/prop/header-bg-image.jpg?t=1472431690);
-			background-position: center;
-			background-repeat: no-repeat;
-			background-attachment: fixed;
-			background-size: cover;
-			-moz-background-size: cover;
-			-webkit-background-size: cover;
-			position: fixed;
-			top: 0;
-			bottom: 0;
-			right: 0;
-			left: 0;
-		}
-
-		.login-screen:before {
-			content: "";
-			background: rgba(0,0,0, 0.5);
-			position: absolute;
-			width: 100%;
-			height: 100%;
-			top: 0;
-			left: 0;
-		}
-
-		.login-center {
-			text-align: left;
-		}
-
-		.login {
-			width: 320px;
-			color: #fff;
-		}
-
-		.login .login-form {
-			text-align: left;
-		}
-
-		.login label {
-			color: #fff;
-		}
-
-		.login-form .input-group .form-control {
-			background: none;
-			height: 44px;
-			color: #ddd;
-		}
-
-		.login-form .input-group .input-group-addon {
-			background: none;
-		}
-
-		.login-form .input-group .input-group-addon .glyphicon {
-			color: #1b5a7c;
-		}
-
-		.login-form .input-group .form-control::-moz-placeholder {
-			color: #fff;
-			opacity: 0.3;
-		}
-
-		.login .sign-btn input.btn {
-			background: #1b5a7c;
-			border-color: #1b5a7c;
-			color: #fff;
-			width: 180px;
-		}
-
-		.login .sign-btn a {
-			text-decoration: none;
-		}
-
-		.login .checkbox {
-			margin-top: 20px;
-			margin-bottom: 20px;
-		}
-
-		.login .forgot {
-			display: inline-block;
-			margin: 20px 0;
+			background-image: url("1.jpg");
 		}
 	</style>
-	<script>
-		$().ready(function() {
-			$("#card").flip({
-				trigger : 'manual'
-			});
-		});
+	<body>
 
-		$(".signup_link").click(function() {
-
-			$(".signin_form").css('opacity', '0');
-			$(".signup_form").css('opacity', '100');
-
-			$("#card").flip(true);
-
-			return false;
-		});
-
-		$("#unflip-btn").click(function() {
-
-			$(".signin_form").css('opacity', '100');
-			$(".signup_form").css('opacity', '0');
-
-			$("#card").flip(false);
-
-			return false;
-
-		});
-	</script>
-	<body class="main">
-
-		<div class="login-screen"></div>
-		<div class="login-center">
-			<div class="container min-height" style="margin-top: 150px;">
+		<!-- Top content -->
+		<div class="top-content">
+			<div class="container">
 				<div class="row">
-					<div align="center">
-						<div class="login" id="card">
-							<div class="front signin_form">
-								<p>
-									Σύνδεση στον λογαριασμό σας
-								</p>
-								<form class="login-form">
-									<div class="form-group">
-										<div class="input-group">
-											<input type="email" class="form-control" name="email" placeholder="Ηλεκτρονική Διεύθυνση">
-											<span class="input-group-addon"> <i class="glyphicon glyphicon-user"></i> </span>
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="input-group">
-											<input type="password" class="form-control" name="password" placeholder="Κωδικός">
-											<span class="input-group-addon"> <i class="glyphicon glyphicon-lock"></i> </span>
-										</div>
-									</div>
-									<div class="checkbox">
-										<label>
-											<input type="checkbox">
-											Remember me next time.</label>
-									</div>
+					<div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 form-box">
+						<form role="form" action="" method="post" class="f1">
+							<img src="logo_el.png">
+							<br>
+							<br>
+							<br>
+							<h3><strong>Συνδεση</strong></h3>
+							<p>
+								Συμπληρώστε τα στοιχεία σας για να συνδεθείτε στον λογαριασμό σας
+							</p>
 
-									<div class="form-group sign-btn">
-										<input type="submit" class="btn" value="Log in">
-										<p>
-											<a href="#" class="forgot">Πρόβλημα με τον λογαριασμό σας?</a>
-										</p>
-										<p>
-											<strong>Αν δεν έχεις λογαριασμό, δημιούργησε εδώ</strong>
-											<br>
-											<a href="signuppsnew.html" id="flip-btn" class="signup signup_link">Εγγραφή</a>
-										</p>
-									</div>
-								</form>
-							</div>
-							<div class="back signup_form" style="opacity: 0;">
+							<fieldset>
+								<div class="form-group">
+									<label class="sr-only" for="id">Ταυτότητα</label>
+									<input type="number" name="id" placeholder="Ταυτότητα" class="form-control" id="id">
+								</div>
+								<div class="form-group">
+									<label class="sr-only" for="password">Κωδικός</label>
+									<input type="password" name="password" placeholder="Κωδικός" class="form-control" id="password">
+								</div>
 								<p>
-									Sign Up for Your New Account
+									<strong>Αν δεν έχεις λογαριασμό, δημιούργησε εδώ</strong>
+									<br>
+									<a href="signupfinal.php" id="flip-btn" class="signup signup_link">Εγγραφή</a>
 								</p>
-								<form class="login-form">
-									<div class="form-group">
-										<div class="input-group">
-											<input type="text" class="form-control" placeholder="">
-											<span class="input-group-addon"> <i class="glyphicon glyphicon-user"></i> </span>
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="input-group">
-											<input type="text" class="form-control">
-											<span class="input-group-btn">
-												<button type="button" class="btn btn-cyan">
-													<span class="fa fa-refresh"></span>
-												</button></span>
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="input-group">
-											<input type="password" class="form-control" placeholder="Confirm Password">
-											<span class="input-group-addon"> <i class="glyphicon glyphicon-lock"></i> </span>
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="input-group">
-											<input type="email" class="form-control" placeholder="Email">
-											<span class="input-group-addon"> <i class="glyphicon glyphicon-envelope"></i> </span>
-										</div>
-									</div>
+								<div class="f1-buttons">
+									<button type="submit" class="btn btn-submit">
+										Submit
+									</button>
+								</div>
+							</fieldset>
 
-									<div class="form-group sign-btn">
-										<input type="submit" class="btn" value="Sign up">
-										<br>
-										<br>
-										<p>
-											You have already Account So <a href="#" id="unflip-btn" class="signup">Log in</a>
-										</p>
-									</div>
-								</form>
-							</div>
-						</div>
+						</form>
 					</div>
 				</div>
+
 			</div>
 		</div>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jQuery-Flip/1.0.18/jquery.flip.js"></script>
 
+		<!-- Javascript -->
+		<script src="bootzard-bootstrap-wizard-template/assets/js/jquery-1.11.1.min.js"></script>
+		<script src="bootzard-bootstrap-wizard-template/assets/bootstrap/js/bootstrap.min.js"></script>
+		<script src="bootzard-bootstrap-wizard-template/assets/js/jquery.backstretch.min.js"></script>
+		<script src="bootzard-bootstrap-wizard-template/assets/js/retina-1.1.0.min.js"></script>
+		<script src="bootzard-bootstrap-wizard-template/assets/js/scripts.js"></script>
+
+		<!--[if lt IE 10]>
+		<script src="assets/js/placeholder.js"></script>
+		<![endif]-->
 	</body>
 </html>
 
