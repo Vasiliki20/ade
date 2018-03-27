7d28960474c9454138d24dde8aee546635bc83ba
Vasiliki <vpante02@cs.ucy.ac.cy>
2018-03-28 01:10:24 +0300

    upd

--- A.D.E/narrow/psindex.php
+++ A.D.E/narrow/psindex.php
@@ -286,108 +286,108 @@
 					<!-- /.col-lg-12 -->
 				</div>
 				<!-- /.row -->
+				<!--	<div class="row">
+				<div class="col-lg-3 col-md-6">
+				<div class="panel panel-primary">
+				<div class="panel-heading">
 				<div class="row">
-					<div class="col-lg-3 col-md-6">
-						<div class="panel panel-primary">
-							<div class="panel-heading">
-								<div class="row">
-									<div class="col-xs-3">
-										<i class="fa fa-comments fa-5x"></i>
-									</div>
-									<div class="col-xs-9 text-right">
-										<div class="huge">
-											26
-										</div>
-										<div>
-											New Comments!
-										</div>
-									</div>
-								</div>
-							</div>
-							<a href="#">
-							<div class="panel-footer">
-								<span class="pull-left">View Details</span>
-								<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-								<div class="clearfix"></div>
-							</div> </a>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-6">
-						<div class="panel panel-green">
-							<div class="panel-heading">
-								<div class="row">
-									<div class="col-xs-3">
-										<i class="fa fa-tasks fa-5x"></i>
-									</div>
-									<div class="col-xs-9 text-right">
-										<div class="huge">
-											12
-										</div>
-										<div>
-											New Tasks!
-										</div>
-									</div>
-								</div>
-							</div>
-							<a href="#">
-							<div class="panel-footer">
-								<span class="pull-left">View Details</span>
-								<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-								<div class="clearfix"></div>
-							</div> </a>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-6">
-						<div class="panel panel-yellow">
-							<div class="panel-heading">
-								<div class="row">
-									<div class="col-xs-3">
-										<i class="fa fa-shopping-cart fa-5x"></i>
-									</div>
-									<div class="col-xs-9 text-right">
-										<div class="huge">
-											124
-										</div>
-										<div>
-											New Orders!
-										</div>
-									</div>
-								</div>
-							</div>
-							<a href="#">
-							<div class="panel-footer">
-								<span class="pull-left">View Details</span>
-								<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-								<div class="clearfix"></div>
-							</div> </a>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-6">
-						<div class="panel panel-red">
-							<div class="panel-heading">
-								<div class="row">
-									<div class="col-xs-3">
-										<i class="fa fa-support fa-5x"></i>
-									</div>
-									<div class="col-xs-9 text-right">
-										<div class="huge">
-											13
-										</div>
-										<div>
-											Support Tickets!
-										</div>
-									</div>
-								</div>
-							</div>
-							<a href="#">
-							<div class="panel-footer">
-								<span class="pull-left">View Details</span>
-								<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-								<div class="clearfix"></div>
-							</div> </a>
-						</div>
-					</div>
+				<div class="col-xs-3">
+				<i class="fa fa-comments fa-5x"></i>
 				</div>
+				<div class="col-xs-9 text-right">
+				<div class="huge">
+				26
+				</div>
+				<div>
+				New Comments!
+				</div>
+				</div>
+				</div>
+				</div>
+				<a href="#">
+				<div class="panel-footer">
+				<span class="pull-left">View Details</span>
+				<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+				<div class="clearfix"></div>
+				</div> </a>
+				</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+				<div class="panel panel-green">
+				<div class="panel-heading">
+				<div class="row">
+				<div class="col-xs-3">
+				<i class="fa fa-tasks fa-5x"></i>
+				</div>
+				<div class="col-xs-9 text-right">
+				<div class="huge">
+				12
+				</div>
+				<div>
+				New Tasks!
+				</div>
+				</div>
+				</div>
+				</div>
+				<a href="#">
+				<div class="panel-footer">
+				<span class="pull-left">View Details</span>
+				<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+				<div class="clearfix"></div>
+				</div> </a>
+				</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+				<div class="panel panel-yellow">
+				<div class="panel-heading">
+				<div class="row">
+				<div class="col-xs-3">
+				<i class="fa fa-shopping-cart fa-5x"></i>
+				</div>
+				<div class="col-xs-9 text-right">
+				<div class="huge">
+				124
+				</div>
+				<div>
+				New Orders!
+				</div>
+				</div>
+				</div>
+				</div>
+				<a href="#">
+				<div class="panel-footer">
+				<span class="pull-left">View Details</span>
+				<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+				<div class="clearfix"></div>
+				</div> </a>
+				</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+				<div class="panel panel-red">
+				<div class="panel-heading">
+				<div class="row">
+				<div class="col-xs-3">
+				<i class="fa fa-support fa-5x"></i>
+				</div>
+				<div class="col-xs-9 text-right">
+				<div class="huge">
+				13
+				</div>
+				<div>
+				Support Tickets!
+				</div>
+				</div>
+				</div>
+				</div>
+				<a href="#">
+				<div class="panel-footer">
+				<span class="pull-left">View Details</span>
+				<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+				<div class="clearfix"></div>
+				</div> </a>
+				</div>
+				</div>
+				</div> -->
 				<!-- /.row -->
 				<div class="row">
 					<div class="col-lg-8">
@@ -405,35 +405,52 @@
 						</div>
 
 					</div>
-						<!-- /.panel -->
+					<!-- /.panel -->
+
+					<!-- /.col-lg-8 -->
+
+					<div class="col-lg-4">
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<i class="fa fa-pencil"></i> Appointments <div class="pull-right"></div>
+							</div>
+							<div>
+								<br>
+							</div>
+							<div align="center">
+								<button type="submit" name="submit" onclick="showadd()">
+									Add appointment
+								</button>
+							</div>
+							<div>
+								<br>
+							</div>
+						</div>
+					</div>
+
+					<!-- /.row -->
 				</div>
-				<!-- /.col-lg-8 -->
-				<div class="col-lg-4"></div>
-				<!-- /.col-lg-4 -->
+				<!-- /#page-wrapper -->
+
 			</div>
-			<!-- /.row -->
-		</div>
-		<!-- /#page-wrapper -->
+			<!-- /#wrapper -->
 
-		</div>
-		<!-- /#wrapper -->
+			<!-- jQuery -->
+			<script src="bootstrap/vendor/jquery/jquery.min.js"></script>
 
-		<!-- jQuery -->
-		<script src="bootstrap/vendor/jquery/jquery.min.js"></script>
+			<!-- Bootstrap Core JavaScript -->
+			<script src="bootstrap/vendor/bootstrap/js/bootstrap.min.js"></script>
 
-		<!-- Bootstrap Core JavaScript -->
-		<script src="bootstrap/vendor/bootstrap/js/bootstrap.min.js"></script>
+			<!-- Metis Menu Plugin JavaScript -->
+			<script src="bootstrap/vendor/metisMenu/metisMenu.min.js"></script>
 
-		<!-- Metis Menu Plugin JavaScript -->
-		<script src="bootstrap/vendor/metisMenu/metisMenu.min.js"></script>
+			<!-- Morris Charts JavaScript -->
+			<script src="bootstrap/vendor/raphael/raphael.min.js"></script>
+			<script src="bootstrap/vendor/morrisjs/morris.min.js"></script>
+			<script src="bootstrap/data/morris-data.js"></script>
 
-		<!-- Morris Charts JavaScript -->
-		<script src="bootstrap/vendor/raphael/raphael.min.js"></script>
-		<script src="bootstrap/vendor/morrisjs/morris.min.js"></script>
-		<script src="bootstrap/data/morris-data.js"></script>
-
-		<!-- Custom Theme JavaScript -->
-		<script src="bootstrap/dist/js/sb-admin-2.js"></script>
+			<!-- Custom Theme JavaScript -->
+			<script src="bootstrap/dist/js/sb-admin-2.js"></script>
 
 	</body>
 
