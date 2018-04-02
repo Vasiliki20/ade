ed76859a31fada0ca3ed3c81dd1e31b666a0a84b
Vasiliki <vpante02@cs.ucy.ac.cy>
2018-04-02 18:39:24 +0300

    upd

--- A.D.E/narrow/waitinglist.php
+++ A.D.E/narrow/waitinglist.php
@@ -352,6 +352,9 @@ $postfields=http_build_query(array(
 										<a href="myclients.php">My clients</a>
 									</li>
 									<li>
+										<a href="myappointments.php">My appointments</a>
+									</li>
+									<li>
 										<a href="waitinglist.php">Waiting List</a>
 									</li>
 									<li>
