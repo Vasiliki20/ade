3a39c6ab68c1b45e1437f70565eaf63641b04499
Vasiliki <vpante02@cs.ucy.ac.cy>
2018-04-04 13:35:51 +0300

    upd

--- A.D.E/narrow/reports.php
+++ A.D.E/narrow/reports.php
@@ -320,8 +320,87 @@
 					</div>
 					<ul>
 						<li>
-							<a href="report1.php">Στατιστικά Συνεδριάσεων</a>
+							<a href="report1.php">Schedules</a>
 						</li>
+						<li>
+							<a href="report1.php">Appointment Search</a>
+						</li>
+						<li>
+							<a href="report1.php">Counselor Notification Worksheet</a>
+						</li>
+						<li>
+							<a href="report1.php">Appointments Missing Attendance</a>
+						</li>
+						<li>
+							<a href="report1.php">Appointment Statistics</a>
+						</li>
+						<li>
+							<a href="report1.php">Monthly Statistics</a>
+						</li>
+						<li>
+							<a href="report1.php">Group Attendance By Appointment Code</a>
+						</li>
+						<li>
+							<a href="report1.php">Group Attendance By Counselor</a>
+						</li>
+						<li>
+							<a href="report1.php">Group Sign Up Sheets</a>
+						</li>
+						<li>
+							<a href="report1.php">Client Profile</a>
+						</li>
+						<li>
+							<a href="report1.php">Client Address List</a>
+						</li>
+						<li>
+							<a href="report1.php">Client File Labels</a>
+						</li>
+						<li>
+							<a href="report1.php">Client Flags</a>
+						</li>
+						<li>
+							<a href="report1.php">Note Statistics</a>
+						</li>
+						<li>
+							<a href="report1.php">Data Form Statistics</a>
+						</li>
+						<li>
+							<a href="report1.php">Data Form Statistics By Appointment</a>
+						</li>
+						<li>
+							<a href="report1.php">Diagnosis Prevalence</a>
+						</li>
+						<li>
+							<a href="report1.php">DSM-IV Score Comparison</a>
+						</li>
+						<li>
+							<a href="report1.php">Waitlist Statistics</a>
+						</li>
+						<li>
+							<a href="report1.php">Intake Wait Time</a>
+						</li>
+						<li>
+							<a href="report1.php">Counselor Activity</a>
+						</li>
+						<li>
+							<a href="report1.php">Counselor Case Load</a>
+						</li>
+						<li>
+							<a href="report1.php">Client Contacts By Counselor</a>
+						</li>
+						<li>
+							<a href="report1.php">Outstanding Tasks</a>
+						</li>
+						<li>
+							<a href="report1.php">Revenue Summary</a>
+						</li>
+						<li>
+							<a href="report1.php">Revenue By Billing Code</a>
+						</li>
+						<li>
+							<a href="report1.php">Export Data</a>
+						</li>
+						
 					</ul>
 				</div>
 
