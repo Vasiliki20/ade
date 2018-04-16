169d23253d1b7bf94580f05188f45d14837518d1
Vasiliki <vpante02@cs.ucy.ac.cy>
2018-04-16 22:27:25 +0300

    upd

--- A.D.E/narrow/therapists.php
+++ A.D.E/narrow/therapists.php
@@ -123,7 +123,7 @@
 							<button type="button" class="close" data-dismiss="modal">
 								&times;
 							</button>
-							<h4 class="modal-title">Add Therapist</h4>
+							<h4 class="modal-title">Add New Therapist</h4>
 						</div>
 						<div class="modal-body">
 							<p>
