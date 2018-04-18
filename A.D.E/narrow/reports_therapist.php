4ab4e6b75fe10f45506fc76f085a63cf3051a8ef
Vasiliki <vpante02@cs.ucy.ac.cy>
2018-04-18 14:55:38 +0300

    upd

---
+++ A.D.E/narrow/reports_therapist.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Κέντρο Ψυχικής Υγείας</title>
+
+		<!-- Bootstrap Core CSS -->
+		<link href="bootstrap/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- MetisMenu CSS -->
+		<link href="bootstrap/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+		<!-- DataTables CSS -->
+		<link href="bootstrap/vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+		<!-- DataTables Responsive CSS -->
+		<link href="bootstrap/vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+
+		<!-- Custom CSS -->
+		<link href="bootstrap/dist/css/sb-admin-2.css" rel="stylesheet">
+
+		<!-- Custom Fonts -->
+		<link href="bootstrap/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	</head>
+	<script>
+		$(document).ready(function() {
+			$('myTable').DataTable();
+		});
+	</script>
+
+	<style>
+		table, th, td {
+			border: 1px solid black;
+			border-collapse: collapse;
+		}
+		th, td {
+			padding: 5px;
+			text-align: left;
+		}
+		label {
+			display: inline-block;
+			width: 140px;
+			text-align: left;
+			float: left;
+		}​
+
+		input {
+			display: inline-block;
+			float: right;
+			text-align: right;
+		}
+	</style>
+
+	<body>
+
+		<div id="wrapper">
+
+			<!-- Navigation -->
+			<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="psindex.php">Κέντρο Ψυχικής Υγείας</a>
+				</div>
+				<!-- /.navbar-header -->
+
+				<ul class="nav navbar-top-links navbar-right">
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#"> <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i> </a>
+						<ul class="dropdown-menu dropdown-user">
+							<li>
+								<a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="psychlogin.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+							</li>
+						</ul>
+						<!-- /.dropdown-user -->
+					</li>
+					<!-- /.dropdown -->
+				</ul>
+				<!-- /.navbar-top-links -->
+				<div class="navbar-default sidebar" role="navigation">
+					<div class="sidebar-nav navbar-collapse">
+						<ul class="nav" id="side-menu">
+							<li>
+								<a href="psindex_therapist.php"><i class="fa fa-table"></i> Calendar</a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-list"></i> Open<span class="fa arrow"></span></a>
+								<ul class="nav nav-second-level">
+									<li>
+										<a href="tasklist_therapist.php">Task List</a>
+									</li>
+									<li>
+										<a href="myclients_therapist.php">My clients</a>
+									</li>
+									<li>
+										<a href="myappointments_therapist.php">My appointments</a>
+									</li>
+									<li>
+										<a href="waitinglist_therapist.php">Waiting List</a>
+									</li>
+									<li>
+										<a href="search_therapist.php">Search</a>
+									</li>
+								</ul>
+								<!-- /.nav-second-level -->
+							</li>
+							<li>
+								<a href="formupload_therapist.php"><i class="fa fa-upload"></i> Upload file</a>
+							</li>
+							<li>
+								<a href="reports_therapist.php"><i class="fa fa-bar-chart-o"></i> Reports</a>
+							</li>
+						</ul>
+					</div>
+					<!-- /.sidebar-collapse -->
+				</div>
+				<!-- /.navbar-static-side -->
+			</nav>
+			<div id="page-wrapper">
+				<div class="row">
+					<div class="col-lg-12">
+						<h1 class="page-header">Reports</h1>
+					</div>
+					<!-- /.col-lg-12 -->
+				</div>
+				<!-- /.row -->
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						Reports
+						<br>
+					</div>
+					<div>
+						<br>
+					</div>
+					<ul>
+						<li>
+							<a href="report1.php">Schedules</a>
+						</li>
+						<li>
+							<a href="report1.php">Appointment Search</a>
+						</li>
+						<li>
+							<a href="report1.php">Counselor Notification Worksheet</a>
+						</li>
+						<li>
+							<a href="report1.php">Appointments Missing Attendance</a>
+						</li>
+						<li>
+							<a href="report1.php">Appointment Statistics</a>
+						</li>
+						<li>
+							<a href="report1.php">Monthly Statistics</a>
+						</li>
+						<li>
+							<a href="report1.php">Group Attendance By Appointment Code</a>
+						</li>
+						<li>
+							<a href="report1.php">Group Attendance By Counselor</a>
+						</li>
+						<li>
+							<a href="report1.php">Group Sign Up Sheets</a>
+						</li>
+						<li>
+							<a href="report1.php">Client Profile</a>
+						</li>
+						<li>
+							<a href="report1.php">Client Address List</a>
+						</li>
+						<li>
+							<a href="report1.php">Client File Labels</a>
+						</li>
+						<li>
+							<a href="report1.php">Client Flags</a>
+						</li>
+						<li>
+							<a href="report1.php">Note Statistics</a>
+						</li>
+						<li>
+							<a href="report1.php">Data Form Statistics</a>
+						</li>
+						<li>
+							<a href="report1.php">Data Form Statistics By Appointment</a>
+						</li>
+						<li>
+							<a href="report1.php">Diagnosis Prevalence</a>
+						</li>
+						<li>
+							<a href="report1.php">DSM-IV Score Comparison</a>
+						</li>
+						<li>
+							<a href="report1.php">Waitlist Statistics</a>
+						</li>
+						<li>
+							<a href="report1.php">Intake Wait Time</a>
+						</li>
+						<li>
+							<a href="report1.php">Counselor Activity</a>
+						</li>
+						<li>
+							<a href="report1.php">Counselor Case Load</a>
+						</li>
+						<li>
+							<a href="report1.php">Client Contacts By Counselor</a>
+						</li>
+						<li>
+							<a href="report1.php">Outstanding Tasks</a>
+						</li>
+						<li>
+							<a href="report1.php">Revenue Summary</a>
+						</li>
+						<li>
+							<a href="report1.php">Revenue By Billing Code</a>
+						</li>
+						<li>
+							<a href="report1.php">Export Data</a>
+						</li>
+						
+					</ul>
+				</div>
+
+				<!-- /#wrapper -->
+
+				<!-- jQuery -->
+				<script src="bootstrap/vendor/jquery/jquery.min.js"></script>
+
+				<!-- Bootstrap Core JavaScript -->
+				<script src="bootstrap/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+				<!-- Metis Menu Plugin JavaScript -->
+				<script src="bootstrap/vendor/metisMenu/metisMenu.min.js"></script>
+
+				<!-- DataTables JavaScript -->
+				<script src="bootstrap/vendor/datatables/js/jquery.dataTables.min.js"></script>
+				<script src="bootstrap/vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+				<script src="bootstrap/vendor/datatables-responsive/dataTables.responsive.js"></script>
+
+				<!-- Custom Theme JavaScript -->
+				<script src="bootstrap/dist/js/sb-admin-2.js"></script>
+
+				<!-- Page-Level Demo Scripts - Tables - Use for reference -->
+				<script>
+					$(document).ready(function() {
+						$('#dataTables-example').DataTable({
+							responsive : true
+						});
+					});
+				</script>
+
+	</body>
+
+</html>
+
+
+<?php
+require_once("requests.php");
+$url="http://thesis.in.cs.ucy.ac.cy/mhc/mhcserver/post/register.php";
+$method='POST';
+if(isset($_POST['submit'])){
+$postfields=http_build_query(array(
+		'id' => $_POST['id'],
+		'email' => $_POST['email'],
+		'name' => $_POST['name'],
+		'lastname' => $_POST['surname'],
+		'password' => $_POST['password']
+	));
+	if(isset($_COOKIE['token'])){
+		$response=request($url,$method,$postfields,$_COOKIE['token']);
+	}else{
+		$response=0;
+	}
+	while($response['status']!=1){
+		$tok=giveToken();
+		print "<h5>".$tok."</h5>";
+		?>
+		<script>
+			document.cookie='token=<?= $tok ?>';
+		</script>
+		<?php
+		//$GLOBALS['curtoken']=giveToken();
+		//print "<h5>".$GLOBALS['curtoken']."</h5>";
+		$response=request($url,$method,$postfields,$tok);
+	}
+	
+}
+?>
