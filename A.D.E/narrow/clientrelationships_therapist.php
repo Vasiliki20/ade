14d9e415314c054aae7bfee88e1d463367af1672
Vasiliki <vpante02@cs.ucy.ac.cy>
2018-04-18 15:13:31 +0300

    upd

---
+++ A.D.E/narrow/clientrelationships_therapist.php
@@ -0,0 +1,338 @@
+<?php
+require_once("requests.php");
+$url="http://thesis.in.cs.ucy.ac.cy/mhc/mhcserver/get/myclient.php?patientID=".$_GET['patientID'];
+$method='GET';
+//if(isset($_POST['submit'])){
+$postfields=http_build_query(array(
+	));
+	if(isset($_COOKIE['token'])){
+		$response=request($url,$method,$postfields,$_COOKIE['token']);
+	}else{
+		$response=0;
+	}
+	while($response['status']!=1){
+		$tok=giveToken();
+		print "<h5>".$tok."</h5>";
+
+?>
+<script>
+	document.cookie='token=<?= $tok ?>';</script>
+<?php
+//$GLOBALS['curtoken']=giveToken();
+//print "<h5>".$GLOBALS['curtoken']."</h5>";
+$response = request($url, $method, $postfields, $tok);
+}
+var_dump($response);
+//}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Κέντρο Ψυχικής Υγείας</title>
+
+		<!-- Bootstrap Core CSS -->
+		<link href="bootstrap/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- MetisMenu CSS -->
+		<link href="bootstrap/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+		<!-- DataTables CSS -->
+		<link href="bootstrap/vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+		<!-- DataTables Responsive CSS -->
+		<link href="bootstrap/vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+
+		<!-- Custom CSS -->
+		<link href="bootstrap/dist/css/sb-admin-2.css" rel="stylesheet">
+
+		<!-- Custom Fonts -->
+		<link href="bootstrap/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	</head>
+	<script>
+		$(document).ready(function() {
+			$('myTable').DataTable();
+		});
+	</script>
+	<body>
+
+		<div id="wrapper">
+
+			<!-- Navigation -->
+			<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="psindex.php">Κέντρο Ψυχικής Υγείας</a>
+				</div>
+				<!-- /.navbar-header -->
+
+				<ul class="nav navbar-top-links navbar-right">
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#"> <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i> </a>
+						<ul class="dropdown-menu dropdown-user">
+							<li>
+								<a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="psychlogin.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+							</li>
+						</ul>
+						<!-- /.dropdown-user -->
+					</li>
+					<!-- /.dropdown -->
+				</ul>
+				<!-- /.navbar-top-links -->
+				<div class="navbar-default sidebar" role="navigation">
+					<div class="sidebar-nav navbar-collapse">
+						<ul class="nav" id="side-menu">
+							<li>
+								<a href="psindex_therapist.php"><i class="fa fa-table"></i> Calendar</a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-list"></i> Open<span class="fa arrow"></span></a>
+								<ul class="nav nav-second-level">
+									<li>
+										<a href="tasklist_therapist.php">Task List</a>
+									</li>
+									<li>
+										<a href="myclients_therapist.php">My clients</a>
+									</li>
+									<li>
+										<a href="myappointments_therapist.php">My appointments</a>
+									</li>
+									<li>
+										<a href="waitinglist_therapist.php">Waiting List</a>
+									</li>
+									<li>
+										<a href="search_therapist.php">Search</a>
+									</li>
+								</ul>
+								<!-- /.nav-second-level -->
+							</li>
+							<li>
+								<a href="formupload_therapist.php"><i class="fa fa-upload"></i> Upload file</a>
+							</li>
+							<li>
+								<a href="reports_therapist.php"><i class="fa fa-bar-chart-o"></i> Reports</a>
+							</li>
+						</ul>
+					</div>
+					<!-- /.sidebar-collapse -->
+				</div>
+				<!-- /.navbar-static-side -->
+			</nav>
+
+			<div id="page-wrapper">
+				<div class="row">
+					<div class="col-lg-12">
+						<h1 class="page-header">Case File</h1>
+					</div>
+				</div>
+				<div>
+					<!-- /.col-lg-12 -->
+					<nav class="navbar navbar-default">
+						<div class="container-fluid">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+								<ul class="nav navbar-nav">
+									<li>
+										<a href="casenotes_therapist.php=<?=$_GET['patientID'] ?>">Σημειώσεις Προόδου</a>
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Σημειώσεις<span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li>
+												<a href="contactlog_therapist.php?patientID=<?= $_GET['patientID'] ?>">Contact Logs</a>
+											</li>
+										</ul>
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Γενικές πληροφορίες<span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li>
+												<a href="personalinformation_therapist.php?patientID=<?= $_GET['patientID'] ?>">Προσωπικά Στοιχεία Πελάτη</a>
+											</li>
+											<li>
+												<a href="schedule_therapist.php?patientID=<?= $_GET['patientID'] ?>">Διαθέσιμο Πρόγραμμα Πελάτη</a>
+											</li>
+										</ul>
+
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ιατρικές Πληροφορίες <span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li>
+												<a href="clientrelationships_therapist.php?patientID=<?= $_GET['patientID'] ?>">Οικογενειακές Σχέσεις Πελάτη</a>
+											</li>
+											<li>
+												<a href="clientmedication_therapist.php?patientID=<?= $_GET['patientID'] ?>">Φαρμακευτική Αγωγή Πελάτη</a>
+											</li>
+											<li>
+												<a href="medhistory_therapist.php?patientID=<?= $_GET['patientID'] ?>">Medlog</a>
+											</li>
+											<li>
+												<a href="form07_therapist.php">Έκθεση αρχικής αξιολόγησης</a>
+											</li>
+										</ul>
+									</li>
+									<li>
+										<a href="externalinformation_therapist.php?patientID=<?= $_GET['patientID'] ?>">Εξωτερική Πληροφόρηση</a>
+									</li>
+									<li>
+										<a href="billing_therapist.php?patientID=<?= $_GET['patientID'] ?>">Πληρωμές</a>
+									</li>
+								</ul>
+
+							</div><!-- /.navbar-collapse -->
+						</div><!-- /.container-fluid -->
+					</nav>
+				</div>
+
+				<div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							Οικογενειακές σχέσεις πελάτη
+						</div>
+						<div class="panel-body">
+
+							<table id="familytable" style="width:100%">
+								<tr>
+									<th>
+									<center>
+										<label>Όνομα:</label>
+									</center></th>
+									<th>
+									<center>
+										<label>Σχέση:</label>
+									</center></th>
+									<th>
+									<center>
+										<label> Ηλικία:</label>
+									</center></th>
+									<th>
+									<center>
+										<label>Ποιότητα:</label>
+									</center></th>
+									<th><!--	<center>
+									Πρόβλημα:
+									</center></th> -->
+								</tr>
+								<?php for($i=0;$i<count($response['relations']);$i++){ ?>
+<tr>
+<td>
+<input type="text" class="form-control" id="familyrelation" name="name" value=<?=$response['relations'][$i]['relation'] ?>>
+</input></td>
+<td>
+<input type="text" class="form-control" id="familyrelation" name="relationship" value=<?=$response['relations'][$i]['name'] ?>>
+</input></td>
+<td>
+<input type="number" class="form-control" id="familyrelation" name="age" value=<?=$response['relations'][$i]['age'] ?>>
+</input></td>
+<td>
+<input type="text" class="form-control" id="familyrelation" name="quality" value=<?=$response['relations'][$i]['quality'] ?>>
+</input></td>
+<!--<td>
+<input type="text" class="form-control" id="familyrelation" name="problem" value=<?=$response['relations'][$i]['psychoproblem']?>>
+</input></td> -->
+</tr>
+<?php } ?>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div>
+				<br>
+				<br>
+			</div>
+			<!-- /#page-wrapper -->
+
+		</div>
+		<!-- /#wrapper -->
+		</div>
+
+		<!-- jQuery -->
+		<script src="bootstrap/vendor/jquery/jquery.min.js"></script>
+
+		<!-- Bootstrap Core JavaScript -->
+		<script src="bootstrap/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+		<!-- Metis Menu Plugin JavaScript -->
+		<script src="bootstrap/vendor/metisMenu/metisMenu.min.js"></script>
+
+		<!-- DataTables JavaScript -->
+		<script src="bootstrap/vendor/datatables/js/jquery.dataTables.min.js"></script>
+		<script src="bootstrap/vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+		<script src="bootstrap/vendor/datatables-responsive/dataTables.responsive.js"></script>
+
+		<!-- Custom Theme JavaScript -->
+		<script src="bootstrap/dist/js/sb-admin-2.js"></script>
+
+		<!-- Page-Level Demo Scripts - Tables - Use for reference -->
+		<script>
+			$(document).ready(function() {
+				$('#dataTables-example').DataTable({
+					responsive : true
+				});
+			});
+		</script>
+
+	</body>
+
+</html>
+
+<?php
+require_once("requests.php");
+$url="http://thesis.in.cs.ucy.ac.cy/mhc/mhcserver/post/register.php";
+$method='POST';
+if(isset($_POST['submit'])){
+$postfields=http_build_query(array(
+'id' => $_POST['id'],
+'email' => $_POST['email'],
+'name' => $_POST['name'],
+'lastname' => $_POST['surname'],
+'password' => $_POST['password']
+));
+if(isset($_COOKIE['token'])){
+$response=request($url,$method,$postfields,$_COOKIE['token']);
+}else{
+$response=0;
+}
+while($response['status']!=1){
+$tok=giveToken();
+print "<h5>".$tok."</h5>";
+?>
+<script>
+	document.cookie='token=<?= $tok ?>';</script>
+<?php
+//$GLOBALS['curtoken']=giveToken();
+//print "<h5>".$GLOBALS['curtoken']."</h5>";
+$response = request($url, $method, $postfields, $tok);
+}
+
+}
+?>
---
+++ A.D.E/narrow/contactlog_therapist.php
@@ -0,0 +1,368 @@
+<?php
+require_once("requests.php");
+$url="http://thesis.in.cs.ucy.ac.cy/mhc/mhcserver/get/myclient.php?patientID=".$_GET['patientID'];
+$method='GET';
+//if(isset($_POST['submit'])){
+$postfields=http_build_query(array(
+	));
+	if(isset($_COOKIE['token'])){
+		$response=request($url,$method,$postfields,$_COOKIE['token']);
+	}else{
+		$response=0;
+	}
+	while($response['status']!=1){
+		$tok=giveToken();
+		print "<h5>".$tok."</h5>";
+
+?>
+<script>
+	document.cookie='token=<?= $tok ?>';</script>
+<?php
+//$GLOBALS['curtoken']=giveToken();
+//print "<h5>".$GLOBALS['curtoken']."</h5>";
+$response = request($url, $method, $postfields, $tok);
+}
+var_dump($response);
+//}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Κέντρο Ψυχικής Υγείας</title>
+
+		<!-- Bootstrap Core CSS -->
+		<link href="bootstrap/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- MetisMenu CSS -->
+		<link href="bootstrap/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+		<!-- DataTables CSS -->
+		<link href="bootstrap/vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+		<!-- DataTables Responsive CSS -->
+		<link href="bootstrap/vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+
+		<!-- Custom CSS -->
+		<link href="bootstrap/dist/css/sb-admin-2.css" rel="stylesheet">
+
+		<!-- Custom Fonts -->
+		<link href="bootstrap/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	</head>
+	<script>
+		$(document).ready(function() {
+			$('myTable').DataTable();
+		});
+	</script>
+	<body>
+
+		<div id="wrapper">
+
+			<!-- Navigation -->
+			<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="psindex.php">Κέντρο Ψυχικής Υγείας</a>
+				</div>
+				<!-- /.navbar-header -->
+
+				<ul class="nav navbar-top-links navbar-right">
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#"> <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i> </a>
+						<ul class="dropdown-menu dropdown-user">
+							<li>
+								<a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="psychlogin.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+							</li>
+						</ul>
+						<!-- /.dropdown-user -->
+					</li>
+					<!-- /.dropdown -->
+				</ul>
+				<!-- /.navbar-top-links -->
+				<div class="navbar-default sidebar" role="navigation">
+					<div class="sidebar-nav navbar-collapse">
+						<ul class="nav" id="side-menu">
+							<li>
+								<a href="psindex_therapist.php"><i class="fa fa-table"></i> Calendar</a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-list"></i> Open<span class="fa arrow"></span></a>
+								<ul class="nav nav-second-level">
+									<li>
+										<a href="tasklist_therapist.php">Task List</a>
+									</li>
+									<li>
+										<a href="myclients_therapist.php">My clients</a>
+									</li>
+									<li>
+										<a href="myappointments_therapist.php">My appointments</a>
+									</li>
+									<li>
+										<a href="waitinglist_therapist.php">Waiting List</a>
+									</li>
+									<li>
+										<a href="search_therapist.php">Search</a>
+									</li>
+								</ul>
+								<!-- /.nav-second-level -->
+							</li>
+							<li>
+								<a href="formupload_therapist.php"><i class="fa fa-upload"></i> Upload file</a>
+							</li>
+							<li>
+								<a href="reports_therapist.php"><i class="fa fa-bar-chart-o"></i> Reports</a>
+							</li>
+						</ul>
+					</div>
+					<!-- /.sidebar-collapse -->
+				</div>
+				<!-- /.navbar-static-side -->
+			</nav>
+
+			<div id="page-wrapper">
+				<div class="row">
+					<div class="col-lg-12">
+						<h1 class="page-header">Case File</h1>
+					</div>
+				</div>
+				<div>
+					<!-- /.col-lg-12 -->
+					<nav class="navbar navbar-default">
+						<div class="container-fluid">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+
+							<!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+								<ul class="nav navbar-nav">
+									<li>
+										<a href="casenotes_therapist.php?patientID=<?=$_GET['patientID'] ?>">Σημειώσεις Προόδου</a>
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Σημειώσεις<span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li>
+												<a href="contactlog_therapist.php?patientID=<?= $_GET['patientID'] ?>">Contact Logs</a>
+											</li>
+										</ul>
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Γενικές πληροφορίες<span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li>
+												<a href="personalinformation_therapist.php?patientID=<?= $_GET['patientID'] ?>">Προσωπικά Στοιχεία Πελάτη</a>
+											</li>
+											<li>
+												<a href="schedule_therapist.php?patientID=<?= $_GET['patientID'] ?>">Διαθέσιμο Πρόγραμμα Πελάτη</a>
+											</li>
+										</ul>
+
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ιατρικές Πληροφορίες <span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li>
+												<a href="clientrelationships_therapist.php?patientID=<?= $_GET['patientID'] ?>">Οικογενειακές Σχέσεις Πελάτη</a>
+											</li>
+											<li>
+												<a href="clientmedication_therapist.php?patientID=<?= $_GET['patientID'] ?>">Φαρμακευτική Αγωγή Πελάτη</a>
+											</li>
+											<li>
+												<a href="medhistory_therapist.php?patientID=<?= $_GET['patientID'] ?>">Medlog</a>
+											</li>
+											<li>
+												<a href="form07_therapist.php">Έκθεση αρχικής αξιολόγησης</a>
+											</li>
+										</ul>
+									</li>
+									<li>
+										<a href="externalinformation_therapist.php?patientID=<?= $_GET['patientID'] ?>">Εξωτερική Πληροφόρηση</a>
+									</li>
+									<li>
+										<a href="billing_therapist.php?patientID=<?= $_GET['patientID'] ?>">Πληρωμές</a>
+									</li>
+								</ul>
+
+							</div><!-- /.navbar-collapse -->
+						</div><!-- /.container-fluid -->
+					</nav>
+				</div>
+
+				<div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							Contact Log
+						</div>
+						<div class="panel-body">
+							<table id="contactlog" style="width:100%">
+								<tr>
+									<th>
+									<center>
+										<label>Όνομα επαγγελματία:</label>
+									</center></th>
+									<th>
+									<center>
+										<label>Επίθετο επαγγελματία:</label>
+									</center></th>
+									<th>
+									<center>
+										<label>Διεύθυνση:</label>
+									</center></th>
+									<th>
+									<center>
+										<label>Πόλη:</label>
+									</center></th>
+									<th>
+									<center>
+										<label>Τηλέφωνο:</label>
+									</center></th>
+									<th>
+									<center>
+										<label>Ηλεκτρονική Διεύθυνση:</label>
+									</center></th>
+									<th>
+									<center>
+										<label>Λόγος μεταβίβασης:</label>
+									</center></th>
+									<th>
+									<center>
+										<label>Πληροφορίες:</label>
+									</center></th>
+									<th>
+									<center>
+										<label>Χρόνος μέχρι να λήξει η εξουσιοδότηση:</label>
+									</center></th>
+								</tr>
+								<?php for($i=0;$i<count($response['communications']);$i++){
+if(strnatcmp($response['communications'][$i]['typeof'],"Transport infos")==0){ ?>
+
+<tr>
+<td>
+<input type="text" class="form-control" id="contactlog" name="name" value=<?= $response['communications'][$i]['nameofprof'] ?>>
+</input></td>
+<td>
+<input type="text" class="form-control" id="contactlog" name="surname" value=<?= $response['communications'][$i]['lastnameofprof'] ?>>
+</input></td>
+<td>
+<input type="text" class="form-control" id="contactlog" name="address" value=<?= $response['communications'][$i]['addressofprof'] ?>>
+</input></td>
+<td>
+<input type="text" class="form-control" id="contactlog" name="city" value=<?= $response['communications'][$i]['cityofprof'] ?>>
+</input></td>
+<td>
+<input type="number" class="form-control" id="contactlog" name="phone" value=<?= $response['communications'][$i]['telofprof'] ?>>
+</input></td>
+<td>
+<input type="email" class="form-control" id="contactlog" name="email" value=<?= $response['communications'][$i]['emailofprof'] ?>>
+</input></td>
+<td>
+<input type="text" class="form-control" id="contactlog" name="reason" value=<?= $response['communications'][$i]['purposeofinfo'] ?>>
+</input></td>
+<td>
+<input type="text" class="form-control" id="contactlog" name="details" value=<?= $response['communications'][$i]['infototransfer'] ?>>
+</input></td>
+<td>
+<input type="text" class="form-control" id="contactlog" name="time" value=<?= $response['communications'][$i]['timeofexpire'] ?>>
+</input></td>
+</tr>
+<?php }} ?>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div>
+				<br>
+				<br>
+			</div>
+			<!-- /#page-wrapper -->
+
+		</div>
+		<!-- /#wrapper -->
+		</div>
+
+		<!-- jQuery -->
+		<script src="bootstrap/vendor/jquery/jquery.min.js"></script>
+
+		<!-- Bootstrap Core JavaScript -->
+		<script src="bootstrap/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+		<!-- Metis Menu Plugin JavaScript -->
+		<script src="bootstrap/vendor/metisMenu/metisMenu.min.js"></script>
+
+		<!-- DataTables JavaScript -->
+		<script src="bootstrap/vendor/datatables/js/jquery.dataTables.min.js"></script>
+		<script src="bootstrap/vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+		<script src="bootstrap/vendor/datatables-responsive/dataTables.responsive.js"></script>
+
+		<!-- Custom Theme JavaScript -->
+		<script src="bootstrap/dist/js/sb-admin-2.js"></script>
+
+		<!-- Page-Level Demo Scripts - Tables - Use for reference -->
+		<script>
+			$(document).ready(function() {
+				$('#dataTables-example').DataTable({
+					responsive : true
+				});
+			});
+		</script>
+
+	</body>
+
+</html>
+
+<?php
+require_once("requests.php");
+$url="http://thesis.in.cs.ucy.ac.cy/mhc/mhcserver/post/register.php";
+$method='POST';
+if(isset($_POST['submit'])){
+$postfields=http_build_query(array(
+'id' => $_POST['id'],
+'email' => $_POST['email'],
+'name' => $_POST['name'],
+'lastname' => $_POST['surname'],
+'password' => $_POST['password']
+));
+if(isset($_COOKIE['token'])){
+$response=request($url,$method,$postfields,$_COOKIE['token']);
+}else{
+$response=0;
+}
+while($response['status']!=1){
+$tok=giveToken();
+print "<h5>".$tok."</h5>";
+?>
+<script>
+	document.cookie='token=<?= $tok ?>';</script>
+<?php
+//$GLOBALS['curtoken']=giveToken();
+//print "<h5>".$GLOBALS['curtoken']."</h5>";
+$response = request($url, $method, $postfields, $tok);
+}
+
+}
+?>
