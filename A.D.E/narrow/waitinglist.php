a9f823b1d6ad49e407a5563e83af2d7a8d8f8590
Vasiliki <vpante02@cs.ucy.ac.cy>
2018-04-27 11:09:56 +0300

    upd

--- A.D.E/narrow/waitinglist.php
+++ A.D.E/narrow/waitinglist.php
@@ -188,9 +188,6 @@ $postfields=http_build_query(array(
 									<li>
 										<a href="waitinglist.php">Waiting List</a>
 									</li>
-									<li>
-										<a href="search.php">Search</a>
-									</li>
 								</ul>
 								<!-- /.nav-second-level -->
 							</li>
