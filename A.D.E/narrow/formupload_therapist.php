f1214f871f3dbb28a1ad838ccd8f90228724a7fe
Vasiliki <vpante02@cs.ucy.ac.cy>
2018-05-13 22:47:44 +0300

    upd

--- A.D.E/narrow/formupload_therapist.php
+++ A.D.E/narrow/formupload_therapist.php
@@ -108,7 +108,7 @@
 			<div id="page-wrapper">
 				<div class="row">
 					<div class="col-lg-12">
-						<h1 class="page-header">Upload file</h1>
+						<h1 class="page-header">Ανάρτηση Αρχείων</h1>
 					</div>
 					<!-- /.col-lg-12 -->
 				</div>
