a5ce0107023063ec2f5c2a54c8b5ea5703501954
Vasiliki <vpante02@cs.ucy.ac.cy>
2018-05-18 14:28:49 +0300

    upd

--- A.D.E/narrow/form11see_admin.php
+++ A.D.E/narrow/form11see_admin.php
@@ -103,6 +103,7 @@ var_dump($response);
 					<div class="sidebar-nav navbar-collapse">
 						<ul class="nav" id="side-menu">
 							<li>
+
 								<a href="psindex_admin.php"><i class="fa fa-table"></i> Ημερολόγιο</a>
 							</li>
 							<li>
@@ -119,6 +120,7 @@ var_dump($response);
 									</li>
 									<li>
 										<a href="waitinglist_admin.php">Λίστα Αναμονής</a>
+
 									</li>
 								</ul>
 								<!-- /.nav-second-level -->
@@ -133,6 +135,84 @@ var_dump($response);
 			<div id="page-wrapper">
 				<div class="row">
 					<div class="col-lg-12">
+						<h1 class="page-header">Case File</h1>
+					</div>
+				</div>
+				<div>
+					<!-- /.col-lg-12 -->
+					<nav class="navbar navbar-default">
+						<div class="container-fluid">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+								<ul class="nav navbar-nav">
+									<li>
+										<a href="casenotes_admin.php?patientID=<?=$_GET['patientID'] ?>">Σημειώσεις Προόδου</a>
+									</li>
+									<li>
+										<a href="filesofpatient_admin.php?patientID=<?=$_GET['patientID'] ?>">Εκθέσεις/Αναφορές</a>
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Γενικές πληροφορίες<span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li>
+												<a href="personalinformationfull_admin.php?patientID=<?= $_GET['patientID'] ?>">Προσωπικά Στοιχεία Πελάτη</a>
+											</li>
+											<li>
+												<a href="schedule_admin.php?patientID=<?= $_GET['patientID'] ?>">Διαθέσιμο Πρόγραμμα Πελάτη</a>
+											</li>
+											<li>
+												<a href="contactlog_admin.php?patientID=<?= $_GET['patientID'] ?>">Contact Logs</a>
+											</li>
+										</ul>
+
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ιατρικές Πληροφορίες <span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li>
+												<a href="clientrelationships_admin.php?patientID=<?= $_GET['patientID'] ?>">Οικογενειακές Σχέσεις Πελάτη</a>
+											</li>
+											<li>
+												<a href="clientmedication_admin.php?patientID=<?= $_GET['patientID'] ?>">Φαρμακευτική Αγωγή Πελάτη</a>
+											</li>
+											<li>
+												<a href="medhistory_admin.php?patientID=<?= $_GET['patientID'] ?>">Medlog</a>
+											</li>
+										</ul>
+									</li>
+									<li>
+										<a href="externalinformation_admin.php?patientID=<?= $_GET['patientID'] ?>">Εξωτερική Πληροφόρηση</a>
+									</li>
+									<li>
+										<a href="billing_admin.php?patientID=<?= $_GET['patientID'] ?>">Πληρωμές</a>
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Τερματισμός<span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li>
+												<a href="formcomplaints_admin.php?patientID=<?=$_GET['patientID'] ?>">Παράπονα πελάτη</a>
+											</li>
+											<li>
+												<a href="formreasons_admin.php?patientID=<?=$_GET['patientID'] ?>">Λόγοι Τερματισμού</a>
+											</li>
+										</ul>
+									</li>
+								</ul>
+
+							</div><!-- /.navbar-collapse -->
+						</div><!-- /.container-fluid -->
+					</nav>
+				</div>
+
+				<div>
 						<h1 class="page-header"><small>Ανασκόπηση θεραπευτικής διαδικασίας/παρέμβασης</small></h1>
 					</div>
 				</div>
@@ -143,33 +223,33 @@ var_dump($response);
 								<div class="form-group">
 								
 									<label for="date"><label>1. Ημερ. Έναρξης Υπηρεσιών: </label></label>
-									<input type="date" class="form-control" id="date" placeholder="" name="datesubmited" value=<?=$response['anaskopisi'][$_GET['an']]['datesubmited']?>>
+									<input type="date" class="form-control" id="date" placeholder="" name="datesubmited" value="<?=$response['anaskopisi'][$_GET['an']]['datesubmited']?>">
 								</div>
 								<div class="form-group">
 									<label for="eidos"><label>2. Είδος θεραπείας/υπηρεσιών: </label></label>
-									<input type="text" class="form-control" id="eidos" placeholder="" name="kind" value=<?=$response['anaskopisi'][$_GET['an']]['kind']?>>
+									<input type="text" class="form-control" id="eidos" placeholder="" name="kind" value="<?=$response['anaskopisi'][$_GET['an']]['kind']?>">
 								</div>
 								<div class="form-group">
 									<label for="firstissues"><label>3. Αρχικό παρουσιαζόμενο ζήτημα/ζητήματα και επιπρόσθετα παρουσιαζόμενα ζητήματα </label></label>
-									<input type="text" class="form-control" id="firstissues" placeholder="" name="initialproblem" value=<?=$response['anaskopisi'][$_GET['an']]['initialproblem']?>>
+									<input type="text" class="form-control" id="firstissues" placeholder="" name="initialproblem" value="<?=$response['anaskopisi'][$_GET['an']]['initialproblem']?>">
 								</div>
 								<div class="form-group">
 									<label for="issues"><label>4. Παρουσιαζόμενα ζητήματα : </label></label>
-									<input type="text" class="form-control" id="issues" placeholder="" name="problems" value=<?=$response['anaskopisi'][$_GET['an']]['problems']?>>
+									<input type="text" class="form-control" id="issues" placeholder="" name="problems" value="<?=$response['anaskopisi'][$_GET['an']]['problems']?>">
 								</div>
 								<div class="form-group">
 									<label for="brief"><label>5. Σύνοψη Αρχικής Αξιολόγησης:</label></label>
 									<br>
 									<label><em>(ιστορικό, προσωπικά στοιχεία, παράγοντες άγχους, θέματα υγείας και άλλα συναφή θέματα, κλινική/διαγνωστική εντύπωση, λειτουργικότητα, προτεινόμενο πλάνο παρέμβασης) </em></label>
 									<br>
-									<input type="text" class="form-control" id="brief" placeholder="" name="sinopsis" value=<?=$response['anaskopisi'][$_GET['an']]['sinopsis']?>>
+									<input type="text" class="form-control" id="brief" placeholder="" name="sinopsis" value="<?=$response['anaskopisi'][$_GET['an']]['sinopsis']?>">
 								</div>
 								<div class="form-group">
 									<label for="changes"><label>6. Σημαντικές αλλαγές στις προσωπικές και περιβαλλοντικές συνθήκες  κατά την υπό εξέταση περίοδο:</label></label>
 									<br>
 									<label><em>(π.χ. αλλαγές σε εργασία, σπουδές, προσωπικές σχέσεις, συνθήκες διαμονής, υγεία, κ.ο.κ)</em></label>
 									<br>
-									<input type="text" class="form-control" id="changes" placeholder="" name="changesenv" value=<?=$response['anaskopisi'][$_GET['an']]['changesenv']?>>
+									<input type="text" class="form-control" id="changes" placeholder="" name="changesenv" value="<?=$response['anaskopisi'][$_GET['an']]['changesenv']?>">
 								</div>
 								</table>
 								<div class="form-group">
@@ -177,21 +257,21 @@ var_dump($response);
 									<br>
 									<label><em>(Σύνοψη μέχρι στιγμής παρέμβασης/θεραπείας συμπ. αποκλίσεις από αρχικό πλάνο και αιτιολόγηση, δυσκολίες/εμπόδια, σημεία που διευκολύνουν τη θεραπεία, στάση/συμπεριφορά πελάτη κ.ο.κ.) </em></label>
 									<br>
-									<input type="text" class="form-control" id="progress" placeholder="" name="evolution" value=<?=$response['anaskopisi'][$_GET['an']]['evolution']?>>
+									<input type="text" class="form-control" id="progress" placeholder="" name="evolution" value="<?=$response['anaskopisi'][$_GET['an']]['evolution']?>">
 								</div>
 								<div class="form-group">
 									<label for="xeirismos"><label>8. Αλλαγές ως αποτέλεσμα της θεραπείας </label></label>
 									<br>
 									<label><em>(στόχοι που επιτεύχθηκαν, νέες δεξιότητες/συμπεριφορές/συνήθειες, αλλαγές στη λειτουργικότητα, νέα αιτήματα κ.ο.κ.)</em></label>
 									<br>
-									<input type="text" class="form-control" id="xeirismos" placeholder="" name="changes" value=<?=$response['anaskopisi'][$_GET['an']]['changes']?>>
+									<input type="text" class="form-control" id="xeirismos" placeholder="" name="changes" value="<?=$response['anaskopisi'][$_GET['an']]['changes']?>">
 								</div>
 								<div class="form-group">
 									<label for="future"><label>9. Μελλοντικό Πρόγραμμα Θεραπείας – Εισηγήσεις και Προγραμματισμός:</label></label>
 									<br>
 									<label><em>(συμεριλαμβάνονται στόχοι θεραπείας, μέθοδοι-τεχνικές και υπολογιζόμενος χρόνος θεραπείας)</em></label>
 									<br>
-									<input type="text" class="form-control" id="future" placeholder="" name="mellon" value=<?=$response['anaskopisi'][$_GET['an']]['mellon']?>>
+									<input type="text" class="form-control" id="future" placeholder="" name="mellon" value="<?=$response['anaskopisi'][$_GET['an']]['mellon']?>">
 								</div>
 								<div class="form-group">
 									<table style="width:100%">
@@ -203,33 +283,33 @@ var_dump($response);
 										<tr>
 											<td>α. ψυχολογική</td>
 											<td>
-											<input type="text" class="form-control" id="future" placeholder="" name="mellon" value=<?=$response['anaskopisi'][$_GET['an']]['psychologicalfunc']?>>
+											<input type="text" class="form-control" id="future" placeholder="" name="mellon" value="<?=$response['anaskopisi'][$_GET['an']]['psychologicalfunc']?>">
 											</td>
 											
 										</tr>
 										<tr>
 											<td>β. επαγγελματική/ακαδημαϊκή</td>
 											<td>
-											<input type="text" class="form-control" id="future" placeholder="" name="mellon" value=<?=$response['anaskopisi'][$_GET['an']]['professionacademicfunc']?>>
+											<input type="text" class="form-control" id="future" placeholder="" name="mellon" value="<?=$response['anaskopisi'][$_GET['an']]['professionacademicfunc']?>">
 											</td>
 										</tr>
 										<tr>
 											<td>γ. κοινωνική</td>
 											<td>
-											<input type="text" class="form-control" id="future" placeholder="" name="mellon" value=<?=$response['anaskopisi'][$_GET['an']]['socialfunc']?>>
+											<input type="text" class="form-control" id="future" placeholder="" name="mellon" value="<?=$response['anaskopisi'][$_GET['an']]['socialfunc']?>">
 											</td>
 										</tr>
 										<tr>
 											<td>δ. αυτοφροντίδα</td>
 											<td>
-											<input type="text" class="form-control" id="future" placeholder="" name="mellon" value=<?=$response['anaskopisi'][$_GET['an']]['autofix']?>>
+											<input type="text" class="form-control" id="future" placeholder="" name="mellon" value="<?=$response['anaskopisi'][$_GET['an']]['autofix']?>">
 											</td>
 										</tr>
 									</table>
 								</div>
 								<div class="form-group">
 									<label for="more"><label>10. Οποιεσδήποτε άλλες σημαντικές πληροφορίες:</label></label>
-									<input type="text" class="form-control" id="more" placeholder="" name="infos" value=<?=$response['anaskopisi'][$_GET['an']]['infos']?>>
+									<input type="text" class="form-control" id="more" placeholder="" name="infos" value="<?=$response['anaskopisi'][$_GET['an']]['infos']?>">
 								</div>
 								
 							</form>
